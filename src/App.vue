63a4661a17cf0e0435d5c910b2013c0e48b522d4
yeyingwen <1269404767@qq.com>
2019-12-13 16:49:00 +0800

    init shop

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuid v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+  import FooterGuid from './components/FooterGuid/FooterGuid.vue'
+  export default {
+  components:{
+    FooterGuid
+  }
+  }
+</script>
+
+<style lang="stylus" ref="stylesheet/stylus">
+  #app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
